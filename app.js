85cbe52f4d660b0d9d112cfd56838cf540ccfebf
tin funix <tinnbFX16153@funix.edu.vn>
2022-09-30 13:29:28 +0700

    làm song lap1.3

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-//Lab 1.2: Tạo Node Server
-//Tạo một node server, khi truy cập vào http://localhost:3000 thì log ra console toàn bộ thuộc tính của request đến.
+// Lab 1.3: Gửi Response
+// Hiển thị một trang HTML có một thẻ H1 với nội dung "Hello from my Node.js server!" khi truy cập vào http://localhost:3000 
+
 
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+    res.write('</html>');
+    res.end();
+
 });
 
-server.listen(3003);
+server.listen(3000);
 
 
