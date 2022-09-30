fbf2128f36806e03b18b0439353ee0fe9bd18bf9
tin funix <tinnbFX16153@funix.edu.vn>
2022-09-30 15:17:11 +0700

    sua lai lab1.4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Lab 1.4:  Chuyển hướng request
 // Hiển thị một trang HTML có một thẻ input và một button khi truy cập vào http://localhost:3000, bấm button thì chuyển qua http://localhost:3000/message. Khi truy cập http://localhost:3000/message thì hiển thị một trang HTML có một thẻ H1 với nội dung "Hello from my Node.js server!"
 
-
 const http = require('http');
+
 const server = http.createServer((req, res) => {
 
     const url = req.url;
@@ -15,9 +15,11 @@ const server = http.createServer((req, res) => {
         res.write('<body>')
         res.write('<form>')
 
-        res.write('</form action="/message" method="POST">')
+
+        // form sẽ gửi dữ liệu đến địa chỉ trong action
+        res.write('<form action="/message" method="POST">')
         res.write('<input type="text" name="message">');
-        res.write('  <a href="http://localhost:3000/message" >   <button type="submit"> send    </button>        </a>     ')
+        res.write('<button type="submit"> send    </button>')
 
         res.write('</form>')
 
@@ -35,8 +37,6 @@ const server = http.createServer((req, res) => {
     res.write('</body>')
     res.write('</html>')
     res.end();
-
-
 
 })
 
