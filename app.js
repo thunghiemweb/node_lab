00d33c9c6bd11216915e06b73c51b546d65989eb
tin funix <tinnbFX16153@funix.edu.vn>
2022-09-30 16:13:31 +0700

    sua loi from

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const server = http.createServer((req, res) => {
 
     const url = req.url;
     if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
         res.write('<html>')
         res.write('<head>')
         res.write('<title>Enter Message</title>')
