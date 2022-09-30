118c8f795dbbf763c21a6d9da6b4473b191c054d
tin funix <tinnbFX16153@funix.edu.vn>
2022-09-30 14:33:01 +0700

    lam song lab1.4

---
+++ app.js
@@ -0,0 +1,43 @@
+// Lab 1.4:  Chuyển hướng request
+// Hiển thị một trang HTML có một thẻ input và một button khi truy cập vào http://localhost:3000, bấm button thì chuyển qua http://localhost:3000/message. Khi truy cập http://localhost:3000/message thì hiển thị một trang HTML có một thẻ H1 với nội dung "Hello from my Node.js server!"
+
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head>')
+        res.write('<title>Enter Message</title>')
+        res.write('</head>')
+        res.write('<body>')
+        res.write('<form>')
+
+        res.write('</form action="/message" method="POST">')
+        res.write('<input type="text" name="message">');
+        res.write('  <a href="http://localhost:3000/message" >   <button type="submit"> send    </button>        </a>     ')
+
+        res.write('</form>')
+
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head>')
+    res.write('<title>My first page</title>')
+    res.write('</head>')
+    res.write('<body>')
+    res.write('<h1>Hello from my Node.js Server!</h1>')
+    res.write('</body>')
+    res.write('</html>')
+    res.end();
+
+
+
+})
+
+server.listen(3000);
