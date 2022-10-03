206a89938aefdcfe4566c5b3801c6fafb8717990
tin funix <tinnbFX16153@funix.edu.vn>
2022-10-03 14:26:32 +0700

    lam song lab2.8

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-// Lab 2.7: Sử dụng Express Router
-// Cài đặt Express Router và chuyển các xử lý ở app.js về các file Router tương ứng.
+// Lab 2.8: Thêm trang xử lý lỗi 404
+// Thêm middleware ở app.js và bắt những đường dẫn mà chưa được định nghĩa rồi trả về một trang với nội dung: "Page not found".
 
 const express = require('express');
 const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require("./routes/shop")
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require("./routes/shop");
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
+
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
 
